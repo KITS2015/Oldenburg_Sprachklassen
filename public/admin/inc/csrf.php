1d6b348d45dbc82a0436fbb9e7d78d431e429e95
Stefan Kuhlmann <stefan.kuhlmann@kuhlmann-its.de>
2026-01-29 13:53:44 +0100

    Update csrf.php

--- public/admin/inc/csrf.php
+++ public/admin/inc/csrf.php
@@ -1,1 +1,23 @@
+<?php
+declare(strict_types=1);
 
+// Datei: public/admin/inc/csrf.php
+
+function csrf_token(): string
+{
+    if (empty($_SESSION['csrf_token']) || !is_string($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+function csrf_verify(string $token): bool
+{
+    if (!isset($_SESSION['csrf_token']) || !is_string($_SESSION['csrf_token'])) {
+        return false;
+    }
+    if ($token === '') {
+        return false;
+    }
+    return hash_equals($_SESSION['csrf_token'], $token);
+}
