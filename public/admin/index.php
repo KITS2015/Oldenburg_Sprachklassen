5a3b8ffe312a23ed3a8042a569dd5a71c49a92c2
Stefan Kuhlmann <stefan.kuhlmann@kuhlmann-its.de>
2026-01-29 13:33:45 +0100

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 require_once __DIR__ . '/inc/bootstrap.php';
 require_once __DIR__ . '/inc/auth.php';
 
-// Wenn bereits eingeloggt → Dashboard
 if (is_admin_logged_in()) {
     header('Location: /admin/dashboard.php');
     exit;
 }
 
-// Sonst → Login
 header('Location: /admin/login.php');
 exit;
-
