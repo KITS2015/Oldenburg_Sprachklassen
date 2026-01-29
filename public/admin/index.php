be8127446600228af7d19579fec03fd340369050
Stefan Kuhlmann <stefan.kuhlmann@kuhlmann-its.de>
2026-01-29 13:34:36 +0100

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,14 +1,1 @@
-// Datei: public/admin/index.php
-<?php
-declare(strict_types=1);
 
-require_once __DIR__ . '/inc/bootstrap.php';
-require_once __DIR__ . '/inc/auth.php';
-
-if (is_admin_logged_in()) {
-    header('Location: /admin/dashboard.php');
-    exit;
-}
-
-header('Location: /admin/login.php');
-exit;
