2a9eb895e7b5e98c02fbd5f3e462376eab245970
Stefan Kuhlmann <stefan.kuhlmann@kuhlmann-its.de>
2026-01-30 08:50:19 +0100

    Update footer.php

--- public/partials/footer.php
+++ public/partials/footer.php
@@ -4,6 +4,6 @@
   <a href="/datenschutz.php" class="text-decoration-none">Datenschutz</a>
 </footer>
 
-<script src="/assets/bootstrap/css/bootstrap.bundle.min.js"></script>
+<script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
