220be98c8afcdcab04608fc90ac65cde7e7129fe
Stefan Kuhlmann <stefan.kuhlmann@kuhlmann-its.de>
2026-02-03 15:54:02 +0100

    Update header.php

--- public/admin/inc/header.php
+++ public/admin/inc/header.php
@@ -24,18 +24,21 @@ $activeNav = $activeNav ?? '';
 <body class="admin-shell">
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark admin-topbar">
-    <a class="navbar-brand fw-bold" href="/admin/dashboard.php">Sprach-Portal Admin</a>
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold" href="/admin/dashboard.php">Sprach-Portal Admin</a>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminTopNav" aria-controls="adminTopNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminTopNav"
+                aria-controls="adminTopNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="adminTopNav">
-        <ul class="navbar-nav ms-auto">
-            <li class="nav-item">
-                <a class="nav-link" href="/admin/logout.php">Abmelden</a>
-            </li>
-        </ul>
+        <div class="collapse navbar-collapse" id="adminTopNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/admin/logout.php">Abmelden</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 
@@ -46,17 +49,13 @@ $activeNav = $activeNav ?? '';
 
             <div class="list-group list-group-flush">
                 <a class="list-group-item list-group-item-action <?php echo $activeNav==='dashboard'?'active':''; ?>"
-                   href="/admin/dashboard.php">
-                    Dashboard
-                </a>
+                   href="/admin/dashboard.php">Dashboard</a>
+
                 <a class="list-group-item list-group-item-action <?php echo $activeNav==='applications'?'active':''; ?>"
-                   href="/admin/applications.php">
-                    Bewerbungen
-                </a>
+                   href="/admin/applications.php">Bewerbungen</a>
+
                 <a class="list-group-item list-group-item-action <?php echo $activeNav==='bbs'?'active':''; ?>"
-                   href="/admin/bbs.php">
-                    BBS / API-Clients
-                </a>
+                   href="/admin/bbs.php">BBS / API-Clients</a>
             </div>
         </div>
     </aside>
